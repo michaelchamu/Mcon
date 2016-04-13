2c0f2ef1b1c0a17a0d39b55105eb25c15fa85f18
michaelchamu <chamunorwamichael@gmail.com>
2016-04-13 17:12:48 +0100

    Dashboard plus dashboard methods and designs

--- EM_ConProjects/Models/DashboardView.cs
+++ EM_ConProjects/Models/DashboardView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using EM_ConProjects.Models.DataModel;
 
 namespace EM_ConProjects.Models
 {
@@ -12,6 +13,14 @@ namespace EM_ConProjects.Models
         public int totalProjectsComplete { get; set; }
         public double companyEfficiencyRatio { get; set; }
         public int totalUsers { get; set; }
+        public int totalActions { get; set; }
+        public int totalContactors { get; set; }
+        public int totalLocations { get; set; }
+        public int totalActionComplete { get; set; }
+        public int actualSiteVisits { get; set; }
+        public int siteVisits { get; set; }
+        public List<string> locationNames { get; set; }
+        public List<Projects> projects {get; set;}
         
     }
 }
